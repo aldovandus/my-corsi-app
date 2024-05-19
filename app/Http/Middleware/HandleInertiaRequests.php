62f12a8fca7c6493116f36070c8b3e4f81648c83
Aldo Vandus <aldovandus@gmail.com>
2024-05-19 21:02:40 +0200

    chore: add flash messages

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -29,14 +29,19 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        return [
-            ...parent::share($request),
+        return array_merge(parent::share($request), [
             'flash' => [
                 'message' => fn () => $request->session()->get('message')
             ],
             'auth' => [
                 'user' => $request->user(),
             ],
-        ];
+        ]);
+        /*  return [
+            ...parent::share($request),
+            'auth' => [
+                'user' => $request->user(),
+            ],
+        ]; */
     }
 }
