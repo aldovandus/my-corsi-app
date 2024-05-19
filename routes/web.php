931c507c62ef5010d9eb1ddf8cf1d6f916386a29
Aldo Vandus <aldovandus@gmail.com>
2024-05-19 21:02:09 +0200

    chore: add course routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\CourseController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -29,6 +30,11 @@ Route::middleware('auth')->group(function () {
     Route::get('/customers/add', [CustomerController::class, 'add'])->name('customer.add');
     Route::post('/customers/store', [CustomerController::class, 'store'])->name('customer.store');
     Route::delete('/customers/{id}', [CustomerController::class, 'destroy'])->name('customer.destroy');
+    Route::get('/courses', [CourseController::class, 'index'])->name('course.index');
+    Route::get('/courses/add', [CourseController::class, 'add'])->name('course.add');
+    Route::post('/courses/store', [CourseController::class, 'store'])->name('course.store');
+    Route::get('/courses/show/{id}', [CourseController::class, 'show'])->name('course.show');
+    Route::delete('/courses/{id}', [CourseController::class, 'destroy'])->name('course.destroy');
 });
 
 require __DIR__ . '/auth.php';
