60cda65e788c0c93bb6eaea9caa8e2d64ecde21a
Aldo Vandus <aldovandus@gmail.com>
2024-05-20 15:17:20 +0200

    chore: add new components

--- resources/js/components/ui/date-picker.tsx
+++ resources/js/components/ui/date-picker.tsx
@@ -1,17 +1,15 @@
-"use client";
-
 import * as React from "react";
 import { format } from "date-fns";
 import { Calendar as CalendarIcon } from "lucide-react";
 
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
-import { Calendar } from "@/components/ui/calendar";
+import { Button } from "@/Components/ui/button";
+import { Calendar } from "@/Components/ui/calendar";
 import {
     Popover,
     PopoverContent,
     PopoverTrigger,
-} from "@/components/ui/popover";
+} from "@/Components/ui/popover";
 
 import { it } from "date-fns/locale";
 
@@ -20,6 +18,7 @@ const DatePicker = ({
     onChange,
 }: {
     label?: string;
+    onChange?(): void;
 }) => {
     const [date, setDate] = React.useState<Date>();
 
