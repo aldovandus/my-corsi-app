0614406c097559598dff34f893cc8841c44a1c32
Aldo Vandus <aldovandus@gmail.com>
2024-05-25 00:02:45 +0200

    dashboard wip

--- resources/js/Pages/Dashboard.tsx
+++ resources/js/Pages/Dashboard.tsx
@@ -1,6 +1,8 @@
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { Head } from "@inertiajs/react";
 import { PageProps } from "@/types";
+import { Card, CardHeader, CardTitle, CardContent } from "@/Components/ui/card";
+import { DollarSign, Users, CreditCard, Activity } from "lucide-react";
 
 export default function Dashboard({ auth }: PageProps) {
     return (
@@ -17,8 +19,75 @@ export default function Dashboard({ auth }: PageProps) {
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
                     <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">
+                        {/* <div className="p-6 text-gray-900">
                             You're logged in!
+                        </div> */}
+
+                        <div className="grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-4">
+                            <Card x-chunk="dashboard-01-chunk-0">
+                                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                    <CardTitle className="text-sm font-medium">
+                                        Total Revenue
+                                    </CardTitle>
+                                    <DollarSign className="h-4 w-4 text-muted-foreground" />
+                                </CardHeader>
+                                <CardContent>
+                                    <div className="text-2xl font-bold">
+                                        $45,231.89
+                                    </div>
+                                    <p className="text-xs text-muted-foreground">
+                                        +20.1% from last month
+                                    </p>
+                                </CardContent>
+                            </Card>
+                            <Card x-chunk="dashboard-01-chunk-1">
+                                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                    <CardTitle className="text-sm font-medium">
+                                        Subscriptions
+                                    </CardTitle>
+                                    <Users className="h-4 w-4 text-muted-foreground" />
+                                </CardHeader>
+                                <CardContent>
+                                    <div className="text-2xl font-bold">
+                                        +2350
+                                    </div>
+                                    <p className="text-xs text-muted-foreground">
+                                        +180.1% from last month
+                                    </p>
+                                </CardContent>
+                            </Card>
+                            <Card x-chunk="dashboard-01-chunk-2">
+                                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                    <CardTitle className="text-sm font-medium">
+                                        Sales
+                                    </CardTitle>
+                                    <CreditCard className="h-4 w-4 text-muted-foreground" />
+                                </CardHeader>
+                                <CardContent>
+                                    <div className="text-2xl font-bold">
+                                        +12,234
+                                    </div>
+                                    <p className="text-xs text-muted-foreground">
+                                        +19% from last month
+                                    </p>
+                                </CardContent>
+                            </Card>
+                            <Card x-chunk="dashboard-01-chunk-3">
+                                <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+                                    <CardTitle className="text-sm font-medium">
+                                        Active Now
+                                    </CardTitle>
+                                    <Activity className="h-4 w-4 text-muted-foreground" />
+                                </CardHeader>
+                                <CardContent>
+                                    <div className="text-2xl font-bold">
+                                        +573
+                                    </div>
+                                    <p className="text-xs text-muted-foreground">
+                                        +201 since last hour
+                                    </p>
+                                </CardContent>
+                            </Card>
                         </div>
                     </div>
                 </div>
