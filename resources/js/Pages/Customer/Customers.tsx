053655572a9cb1f12e8d850d758c810f29435714
Aldo Vandus <aldovandus@gmail.com>
2024-06-06 21:08:16 +0200

    chore: pass breadcrumbs routes

--- resources/js/Pages/Customer/Customers.tsx
+++ resources/js/Pages/Customer/Customers.tsx
@@ -171,6 +171,7 @@ const Customers = ({
                     Clienti
                 </h2>
             }
+            breadcrumbRoutes={[{ label: "Clienti", url: "customer.index" }]}
         >
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
