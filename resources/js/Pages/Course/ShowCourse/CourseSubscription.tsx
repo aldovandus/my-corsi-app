789494a895c7fd6af0fa9d34cd75ac7496aca9c6
Aldo Vandus <aldovandus@gmail.com>
2024-09-17 15:15:50 +0200

    fix: add

--- resources/js/Pages/Course/ShowCourse/CourseSubscription.tsx
+++ resources/js/Pages/Course/ShowCourse/CourseSubscription.tsx
@@ -11,16 +11,28 @@ import { getInitials } from "@/lib/utils";
 import { CustomerWithSubscription } from "@/types";
 import { Separator } from "@/Components/ui/separator";
 import { Badge } from "@/Components/ui/badge";
+import { router, useForm } from "@inertiajs/react";
+import {
+    DropdownMenu,
+    DropdownMenuTrigger,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+} from "@/Components/ui/dropdown-menu";
+import { MoreHorizontal } from "lucide-react";
+import { Button } from "@/Components/ui/button";
 
 const CourseSubscription = ({
     subscription,
 }: {
     subscription: CustomerWithSubscription;
 }) => {
+    const { delete: destroy } = useForm();
     return (
         <Card>
             <CardHeader className="flex">
-                <div className="flex gap-4 items-center">
+                <div className="flex gap-4 items-center justify-between">
                     <Avatar className="hidden h-14 w-14 sm:flex">
                         <AvatarImage src="/avatars/01.png" alt="Avatar" />
                         <AvatarFallback>
@@ -36,6 +48,51 @@ const CourseSubscription = ({
                         </CardTitle>
                         <div className="text-sm">{subscription.email}</div>
                     </div>
+
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button variant="ghost" className="h-8 w-8 p-0">
+                                <span className="sr-only">Open menu</span>
+                                <MoreHorizontal className="h-4 w-4" />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>Azioni</DropdownMenuLabel>
+
+                            <DropdownMenuItem
+                                className="cursor-pointer"
+                                onClick={() => {
+                                    router.get(
+                                        route("customer.show", {
+                                            id: subscription.customer_id,
+                                        })
+                                    );
+                                }}
+                            >
+                                Vedi
+                            </DropdownMenuItem>
+
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem
+                                className="cursor-pointer"
+                                onClick={() => {
+                                    if (
+                                        confirm(
+                                            "Sei sicuro di voler eliminare l'iscrizione?"
+                                        )
+                                    ) {
+                                        destroy(
+                                            route("subsdcription.destroy", {
+                                                id: subscription.id,
+                                            })
+                                        );
+                                    }
+                                }}
+                            >
+                                Elimina
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
                 </div>
                 <div className="pt-3">
                     <Separator />
