86fd8889e43351d2921b4f8e5b4862dff3c760a4
aldovandus <aldovandus@gmail.com>
2024-10-31 15:35:51 +0100

    fix: formatDate

--- resources/js/lib/hooks/formatDate.tsx
+++ resources/js/lib/hooks/formatDate.tsx
@@ -1,14 +1,15 @@
 import { format } from "date-fns";
 import { it } from "date-fns/locale";
+import { ReactElement, ReactNode } from "react";
 
-function formatDate(date: Date) {
+function formatDate(date: Date, onError?: ReactNode | string) {
     try {
         return format(date, "PPP", { locale: it });
     } catch (error) {
         if (error instanceof RangeError) {
             console.error("Invalid time value:", error);
             // Gestisci l'errore come preferisci, ad esempio ritornando una stringa vuota
-            return "Data non valida";
+            return onError ? onError : "Data non valida";
         } else {
             // Propaga altri errori
             throw error;
